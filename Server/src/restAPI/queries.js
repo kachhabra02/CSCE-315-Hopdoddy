9e3427503c2012380300e9af34261080919b0610
Eliseo Garza <eliseogarza@tamu.edu>
2023-10-26 13:46:17 -0500

    Outlined queries and added them to export list

--- Server/src/restAPI/queries.js
+++ Server/src/restAPI/queries.js
@@ -1,60 +1,55 @@
 
-//Common Queries
+// Common Queries
 const getCatQuery = "SELECT DISTINCT Category FROM Menu WHERE Is_Modification=FALSE AND Is_Available ORDER BY Category ASC";
 const getSubCategoriesQuery = "SELECT DISTINCT Sub_Category FROM Menu WHERE Category= $1 AND Is_Available ORDER BY Sub_Category ASC";
 const getMenuItemsQuery = "SELECT * FROM Menu WHERE Category= $1 AND Sub_Category= $2 AND Is_Available ORDER BY Item_ID ASC";
+// const getModificationsQuery = ; // TODO: Add me
 
-//Manager Queries
+// Manager Queries
+// const viewInventoryQuery = ; // TODO: ADD ME
+// const viewMenuQuery = ; // TODO: ADD ME
+
 const checkInventoryExists =  "SELECT invName FROM Inventory invName WHERE invName.Inventory_Name = $1";
 const addInventoryItemQuery = "INSERT INTO Inventory (Inventory_Name, Price, Quantity, Unit) VALUES ( $1 , $2 , $3 , $4 )";
+// const changeInventoryItemQuery = ; // TODO: ADD ME
 const removeInventoryItemQuery = "UPDATE Inventory Set Is_Available = FALSE WHERE Inventory_Name = $1";
 
 const checkMenuItemExists = "SELECT menName FROM Menu menName WHERE menName.Item_Name = $1";
+// const addMenuItemQuery = ; // TODO: ADD ME
+// const changeMenuItemQuery = ; // TODO: ADD ME
 const removeMenuItemQuery = "UPDATE Menu Set Is_Available = FALSE WHERE Item_Name = $1";
 
-/* TODO: Continuation:
-
-// Common Queries
-
-const getModifications = ; // ADD ME
-
-// Manager Queries
-
-const viewInventory = ; // ADD ME
-const viewMenu = ; // ADD ME
-
-const addInventoryItem = ; 
-const changeInventoryItem = ; // ADD ME
-const removeInventoryItem = ;
-
-const addMenuItem = ; // ADD ME
-const changeMenuItem = ; // ADD ME
-const removeMenuItem = ;
-
 // Report Queries
-const getPopularPairs = ;
-const getOrderHistory = ;
-const getSalesReport = ;
-const getRestockItems = ;
-const getExcessReport = ;
+// const getPopularPairsQuery = ; // TODO: ADD ME
+// const getOrderHistoryQuery = ; // TODO: ADD ME
+// const getSalesReportQuery = ; // TODO: ADD ME
+// const getRestockItemsQuery = ; // TODO: ADD ME
+// const getExcessReportQuery = ; // TODO: ADD ME
 
 // Cashier Queries
+// const placeTransactionQuery = ; // TODO: ADD ME
 
-const placeTransaction = ;
 
-
-// FIXME: Add my queries to the export list
-
-*/
-
-//Exporting Queries
+// Exporting Queries
 module.exports = {
     getCatQuery,
     getSubCategoriesQuery,
     getMenuItemsQuery,
+    // getModificationsQuery,
+    // viewInventoryQuery,
+    // viewMenuQuery,
     checkInventoryExists,
     addInventoryItemQuery,
+    // changeInventoryItemQuery,
     removeInventoryItemQuery,
     checkMenuItemExists,
+    // addMenuItemQuery,
+    // changeMenuItemQuery,
     removeMenuItemQuery,
+    // getPopularPairsQuery,
+    // getOrderHistoryQuery,
+    // getSalesReportQuery,
+    // getRestockItemsQuery,
+    // getExcessReportQuery,
+    // placeTransactionQuery,
 };
