00164d2d544b370778cca2bb3c758dc6ebb800a1
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 04:50:47 -0500

    Added route templates to menu file

--- Server/src/restAPI/menu.js
+++ Server/src/restAPI/menu.js
@@ -5,7 +5,132 @@ const router = express.Router()
 // Retrieve connection pool
 const pool = require('./db');
 
-// TODO
+
+/***** /api/menu *****/
+// Add a menu item
+router.post('/', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+        
+        // TODO: Send queries
+        
+        await client.query('COMMIT');
+      } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+      } finally {
+        client.release();
+      }
+
+      // TODO: Send response
+});
+
+
+/***** /api/menu/item/:id *****/
+// Update menu item
+router.put('/item/:id', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+// Delete menu item
+router.delete('/item/:id', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/menu/categories *****/
+// Get categories
+router.get('/categories', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/menu/sub-categories *****/
+// Get subcategories
+router.get('/sub-categories', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/menu/items *****/
+// Get menu items
+router.get('/items', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/menu/modifications *****/
+// Get modifications
+router.get('/modifications', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/menu/view *****/
+router.get('/view', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
 
 // Export URI router
 module.exports = router
--- Server/src/restAPI/transactions.js
+++ Server/src/restAPI/transactions.js
@@ -5,14 +5,18 @@ const router = express.Router()
 // Retrieve connection pool
 const pool = require('./db');
 
+
+/***** /api/transactions *****/
 // Place a transaction
-router.put('/', async (req, res) => {
+router.post('/', async (req, res) => {
+    // TODO: Get necessary info from request
+
     const client = await pool.connect();
 
     try {
         await client.query('BEGIN');
         
-        // TODO
+        // TODO: Send queries
         
         await client.query('COMMIT');
       } catch (err) {
@@ -21,16 +25,23 @@ router.put('/', async (req, res) => {
       } finally {
         client.release();
       }
+
+      // TODO: Send response
 });
 
 // Get order history
 router.get('/', async (req, res) => {
+    // TODO: Get necessary info from request
+
     const client = await pool.connect();
 
-    // TODO
+    // TODO: Send query
 
     client.release();
+
+    // TODO: Send response
 });
+
 
 // Export URI router
 module.exports = router
