7dcf6cc78502e0e05c0c5cb6c043047ad0f0e37c
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 04:27:08 -0500

    Added route templates to transactions file

--- Server/src/restAPI/transactions.js
+++ Server/src/restAPI/transactions.js
@@ -5,7 +5,32 @@ const router = express.Router()
 // Retrieve connection pool
 const pool = require('./db');
 
-// TODO
+// Place a transaction
+router.put('/', async (req, res) => {
+    const client = await pool.connect();
+
+    try {
+        await client.query('BEGIN');
+        
+        // TODO
+        
+        await client.query('COMMIT');
+      } catch (err) {
+        await client.query('ROLLBACK');
+        throw err;
+      } finally {
+        client.release();
+      }
+});
+
+// Get order history
+router.get('/', async (req, res) => {
+    const client = await pool.connect();
+
+    // TODO
+
+    client.release();
+});
 
 // Export URI router
 module.exports = router
