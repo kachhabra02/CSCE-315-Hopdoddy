b7d46d3ba1ae1e52dca1eaa8b508b506a88d8b2b
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 16:04:26 -0500

    Added environment variables to local project and vercel deployment

--- Server/restAPI/db.js
+++ Server/restAPI/db.js
@@ -1,13 +1,14 @@
 // Necessary requirements
 const Pool = require('pg').Pool;
+require('dotenv').config();
 
 // Add db credentials
 const pool = new Pool({
-    user:"csce315_905_02user",
-    host:"csce-315-db.engr.tamu.edu",
-    database:"csce315_905_02db",
-    password: "Password123",
-    port: 5432
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_DB,
+    password: process.env.DB_PWD,
+    port: process.env.DB_PORT
 });
 
 // Export connection pool
