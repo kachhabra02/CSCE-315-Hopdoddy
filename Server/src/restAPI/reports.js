1c4062c36098e993d474291f6b912fa4f73b6a5d
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 05:48:29 -0500

    Added route templates to reports file

--- Server/src/restAPI/reports.js
+++ Server/src/restAPI/reports.js
@@ -5,7 +5,81 @@ const router = express.Router()
 // Retrieve connection pool
 const pool = require('./db');
 
-// TODO
+
+/***** /api/reports/popular-pairs *****/
+// Generate "What Sells Together?" report
+router.get('/popular-pairs', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/reports/sales *****/
+// Generate sales report
+router.get('/sales', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/reports/excess *****/
+// Generate excess report
+router.get('/excess', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/reports/restock *****/
+// Generate restock report
+router.get('/restock', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
+
+/***** /api/reports/product-usage *****/
+// Generate product usage report
+router.get('/product-usage', async (req, res) => {
+    // TODO: Get necessary info from request
+
+    const client = await pool.connect();
+
+    // TODO: Send query
+
+    client.release();
+
+    // TODO: Send response
+});
+
 
 // Export URI router
 module.exports = router
