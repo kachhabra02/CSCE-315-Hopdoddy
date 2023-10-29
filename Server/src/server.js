b03552d04286c0a1f8dde6044ef98c577e10730c
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 03:25:16 -0500

    Adjust server.js for new route structure

--- Server/src/server.js
+++ Server/src/server.js
@@ -1,18 +1,32 @@
-require('dotenv').config();
+// General app requirements
 const express = require('express');
 const app = express();
-const dbRoutes = require('./restAPI/routes');
-const port = process.env.PORT || 5000
 
+// Configure port to listen on
+require('dotenv').config();
+const port = (process.env.PORT || 5000);
+
+// Import routers for functionality
+const transactions = require('./restAPI/transactions');
+const menu = require('./restAPI/menu');
+const inventory = require('./restAPI/inventory');
+const reports = require('./restAPI/reports');
+
+// Add other middleware to be used
 app.use(express.json());
 
-app.get('/', (req, res) => {
+// Add routes to the app
+app.use("/api/transactions", transactions);
+app.use("/api/menu", menu);
+app.use("/api/inventory", inventory);
+app.use("/api/reports", reports);
+
+// Add base route to test setup
+app.get('/api', (req, res) => {
   res.send('Hello, World!');
 });
 
-app.use("/api/db", dbRoutes);
-
-
+// Begin listening on determined port
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`);
 });
