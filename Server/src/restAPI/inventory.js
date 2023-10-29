81cc7d5c0e4af0d6d7759ef7b389fdebcd9482d4
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-29 04:01:37 -0500

    Improved basic structure of routing files

--- Server/src/restAPI/inventory.js
+++ Server/src/restAPI/inventory.js
@@ -1,6 +1,11 @@
+// Necessary requirements
 const express = require('express')
 const router = express.Router()
 
+// Retrieve connection pool
+const pool = require('./db');
 
+// TODO
 
+// Export URI router
 module.exports = router
