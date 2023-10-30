9f06aa47317f8fd20bee034ddd25d5e86ac8b2aa
Thomas Holt <tmholt02@gmail.com>
2023-10-30 00:48:45 -0500

    Added login Idea and some formatting

--- Client/src/NavBar.js
+++ Client/src/NavBar.js
@@ -6,19 +6,30 @@ const HomeLink = () => <Link to='/'><button className='landing-button'>Landing L
 
 const locationLinksMap = {
     '/' : [MenuLink, HomeLink],
-    '/menu': [MenuLink, HomeLink, HomeLink]
+    '/menu' : [MenuLink, HomeLink],
+    '/login' : null,
+    undefined : ""
 };
 
 function NavBar() {
     const location = useLocation();
 
+    if (locationLinksMap[location.pathname] === null) {
+        return <></>
+    }
+
     return (
-        <div className='navbar_buttons'>
-            {locationLinksMap[location.pathname].map((ButtonComponent, index) => (
-                <React.Fragment key={index}>
-                    <ButtonComponent />
-                </React.Fragment>
-            ))}
+        <div id='navbar'>
+            <h3>
+                Hopdoddy!
+            </h3>
+            <div id='navbar-buttons'>
+                {locationLinksMap[location.pathname].map((ButtonComponent, index) => (
+                    <React.Fragment key={index}>
+                        <ButtonComponent />
+                    </React.Fragment>
+                ))}
+            </div>
         </div>
     )
 }
