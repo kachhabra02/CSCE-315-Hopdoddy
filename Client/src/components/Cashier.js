abb17de3b53efafd636e6d435c3c4b5109f98d7a
Donato-Curvino <donato@tamu.edu>
2023-10-29 23:36:38 -0500

    setup structure for passing data to subcomponents

--- Client/src/components/Cashier.js
+++ Client/src/components/Cashier.js
@@ -5,11 +5,14 @@ import TransactionList from "./cashier-subcomponents/TransactionList.js"
 import ItemList from "./cashier-subcomponents/ItemList.js"
 
 function Cashier() {
+    // will be replaced with categories from API call
+    const TSTcategories = ["Burgers", "Fries", "Drinks", "Salads"];
+
     return (
         <div className="Cashier">
             {/* <NavBar />  */}
             <h1>This is the Cashier page</h1>
-            <CategoryList />
+            <CategoryList categories={TSTcategories}/>
             <SubcategoryList />
             <ItemList />
             <TransactionList />
--- Client/src/components/cashier-subcomponents/CategoryList.js
+++ Client/src/components/cashier-subcomponents/CategoryList.js
@@ -1,14 +1,17 @@
 import React from "react";
 
-function CategoryList() {
+function CategoryList(props) {
+    const categoryButtons = props.categories.map((name) => <li><button>{name}</button></li>)
+
     return (
         <div className="CategoryList">
             <h2>This is the CategoryList</h2>
             <ul>
                 {/* list items in here */}
-                <li>Test Burgers</li>
-                <li>Test Test Fries</li>
-                <li>Test Drinks</li>
+                {/* <li>Test Burgers</li>
+                <li>Test Fries</li>
+                <li>Test Drinks</li> */}
+                {categoryButtons}
             </ul>
         </div>
     );
