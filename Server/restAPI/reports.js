5c38266b69add253a2f67fec4a979a8e6e0efec8
Krish Chhabra <kachhabra02@tamu.edu>
2023-10-30 00:36:55 -0500

    Added what sells together report generation

--- Server/restAPI/reports.js
+++ Server/restAPI/reports.js
@@ -12,16 +12,38 @@ const queries = require('./queries');
 /***** /api/reports/popular-pairs *****/
 // Generate "What Sells Together?" report
 router.get('/popular-pairs', async (req, res) => {
-    // TODO: Get necessary info from request
+    // Get necessary info from request
+    if (!req.query.startTime) {
+        res.status(400).send("Must provide start time (startTime)!");
+        return;
+    }
+
+    if (!req.query.endTime) {
+        res.status(400).send("Must provide end time (endTime)!");
+        return;
+    }
+
+    if (!req.query.limit) {
+        res.status(400).send("Must provide limit!");
+        return;
+    }
+
+    // Send query
+    const queryObj = {
+        text: queries.getPopularPairsQuery,
+        values: [req.query.startTime, req.query.endTime, req.query.limit]
+    };
 
     const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
 
-    // TODO: Send query
-
+        res.status(200).json(results.rows);
+    });
     client.release();
-
-    // TODO: Send response
-    res.send("Generate \"What Sells Together?\" report");
 });
 
 
