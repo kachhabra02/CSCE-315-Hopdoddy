51ccc18e4d35b11aeada853d8b3ea6926734a859
Thomas Holt <tmholt02@gmail.com>
2023-10-30 10:14:56 -0500

    Moved landing into its page

--- Client/src/components/landing/Landing.js
+++ Client/src/components/landing/Landing.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 function Landing() {
   return (
-    <>
+    <div id='landing'>
       <h1>Landing</h1>
       <p>Future landing page</p>
-    </>
+    </div>
   )
 }
 
