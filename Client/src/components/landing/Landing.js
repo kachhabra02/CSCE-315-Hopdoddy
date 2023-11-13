471976b4ad00f91705260d36cf4fe394df924a08
Eliseo Garza <eliseogarza@tamu.edu>
2023-11-13 16:24:34 -0600

    Implemented Weather API & basic card for styling

--- Client/src/components/landing/Landing.js
+++ Client/src/components/landing/Landing.js
@@ -1,16 +1,64 @@
-import { Box } from '@mui/material';
+import { Box, CardMedia } from '@mui/material';
+import { Card, CardHeader, CardContent, Typography} from '@mui/material';
 import './Landing.css';
 import hopdoddyPic from './hopdoddy.jpg';
 
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+
+
+
+function getWeather(setWeatherResult) {
+  const baseURL = "https://api.openweathermap.org/data/2.5/";
+  const zipcode = 77840;
+  const units = "imperial";
+
+  // FIXME: May need to specify language later when implementing Google Translate API
+  fetch(`${baseURL}weather?zip=${zipcode}&units=${units}&appid=${process.env.REACT_APP_OPEN_WEATHER_API}`)
+    .then((res) => res.json())
+      .then((result) => {
+        setWeatherResult(result);
+  })
+  .catch(error => console.error('Error fetching weather data:', error));
+}
+
+function WeatherCard(weatherData) {
+  // Destructuring condition to pass on later
+  const condition = weatherData.weather.length > 0 ? weatherData.weather[0].main : "Not available";
+
+  return (
+    <Card sx={{ minWidth: 275 }}>
+      <CardHeader title="Weather Information" />
+      <CardContent>
+        <Typography variant="h6">Temperature: {weatherData.main.temp}°F</Typography>
+        <Typography variant="body1">Humidity: {weatherData.main.humidity}%</Typography>
+        <Typography variant="body1">Wind Speed: {weatherData.wind.speed} mph</Typography>
+        <Typography variant="body1">Condition: {condition}</Typography>
+        {/* <Typography variant="body1">Chance of Precipitation: {weatherData.main.precipitation}%</Typography> */}
+      </CardContent>
+    </Card>
+  );
+}
 
 function Landing() {
+  const [weatherResult, setWeatherResult] = useState({});
+
+  useEffect(() => {
+    getWeather(setWeatherResult); // Making the API call
+  }, []); // Empty dependency array for the initial render
+
+  // FIXME: Display weather data
+  const weatherDisplay = weatherResult.main ? (
+    WeatherCard(weatherResult)
+  ) : (
+    <p>Loading weather data...</p>
+  );
+
   return (
     <Box id='landing' sx={{bgcolor: 'background.default' }}>
       <img src={hopdoddyPic} id='location-img' alt="Hopdoddy Location" />
       <div id='landing-info'>
         <div id='weather'>
-          This div will contain weather info
+          {weatherDisplay}
         </div>
         <div id='location'>
           This div will contain address info
