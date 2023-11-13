c1e2b2d0a25b37dfb8c2dd5ef38cfd159c723b42
Donato-Curvino <donato@tamu.edu>
2023-11-13 12:49:01 -0600

    Added pictures from menuItemPictures branch

--- Client/src/components/customer-subcomponents/ItemCard.jsx
+++ Client/src/components/customer-subcomponents/ItemCard.jsx
@@ -7,6 +7,18 @@ import Button from "@mui/material/Button";
 function ItemCard({name}) {
     return (
       <Card>
+        <CardMedia sx={{height: 200}}>
+            <img 
+                // Name like Goodnight/Good Cause -> goodnight-good_cause.jpg
+                src={`/images/${name.replace(/\s+/g, '_').replace(/\//g, '-').toLowerCase()}.jpg`}
+                alt={name}
+                onError={(e) => {
+                    e.target.onerror = null; // Prevents looping?
+                    e.target.src = "/images/default.jpg";
+                }}
+                height={200}
+            />
+        </CardMedia>
         <CardContent>
             <Button variant="text">
                 {name}
