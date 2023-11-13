03d044aa9c11b1b6126170adedbdbb0f2e6e5e26
Krish Chhabra <kachhabra02@tamu.edu>
2023-11-13 11:48:03 -0600

    Added function to retrieve excess report

--- Client/src/components/manager/datetime-pages/Excess.js
+++ Client/src/components/manager/datetime-pages/Excess.js
@@ -5,13 +5,39 @@ import { Box } from '@mui/material';
 
 import { registerDateTimePage } from './PageInput';
 
+// import {useEffect, useState} from "react";
+import axios from "axios";
+
+const API = axios.create({
+    baseURL: `${process.env.REACT_APP_API_URL}/api`,
+    timeout: 10000 // 10 second timeout
+});
+
+
+function getExcessRport(startTime) {
+    API.get(`/reports/excess?startTime=${startTime}`)
+        .then((res) => {
+            if (res.status < 300) {
+                console.log(res.data);
+                return res.data;
+            }
+            else {
+                console.log(res.data);
+            }
+        })
+        .catch((error) => {
+            console.log(error);
+        });
+}
+
 function Excess() {
   const { startTime } = useParams();
 
   return (
     <Box>
       {Excess.title}<br/>
-      {startTime}
+      {startTime.toLocaleUpperCase()}<br/>
+      {getExcessRport(new Date(startTime).toISOString())}
     </Box>
   )
 }
