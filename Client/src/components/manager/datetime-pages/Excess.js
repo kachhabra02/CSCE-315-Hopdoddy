0dea8a6d96ad44dfb7d08ef811324034713e8c7f
Krish Chhabra <kachhabra02@tamu.edu>
2023-11-13 12:38:49 -0600

    Generate excess report table with MUI Datatable

--- Client/src/components/manager/datetime-pages/Excess.js
+++ Client/src/components/manager/datetime-pages/Excess.js
@@ -1,43 +1,58 @@
-import React from 'react'
+import React from 'react';
+import MUIDataTable from "mui-datatables";
 import { useParams } from 'react-router-dom';
 
 import { Box } from '@mui/material';
 
 import { registerDateTimePage } from './PageInput';
 
-// import {useEffect, useState} from "react";
-import axios from "axios";
+import {useState} from 'react';
+import axios from 'axios';
 
 const API = axios.create({
-    baseURL: `${process.env.REACT_APP_API_URL}/api`,
-    timeout: 10000 // 10 second timeout
+  baseURL: `${process.env.REACT_APP_API_URL}/api`,
+  timeout: 10000 // 10 second timeout
 });
 
 
-function getExcessRport(startTime) {
-    API.get(`/reports/excess?startTime=${startTime}`)
-        .then((res) => {
-            if (res.status < 300) {
-                console.log(res.data);
-                return res.data;
-            }
-            else {
-                console.log(res.data);
-            }
-        })
-        .catch((error) => {
-            console.log(error);
-        });
-}
+const title = 'Excess Inventory Report';
+
+const columns = ['Inventory ID', 'Name', 'Current Quantity', 'Unit', 'Actual Usage', 'Target Usage'];
+
+const options = {
+  filterType: 'checkbox'
+};
+
 
 function Excess() {
   const { startTime } = useParams();
+  const [data, setData] = useState([]);
+
+  API.get(`/reports/excess?startTime=${(new Date(startTime)).toISOString()}`)
+     .then((res) => {
+       if (res.status < 300) {
+         console.log('Success');
+         setData(res.data.map((item) => [item.inventory_id, item.inventory_name, item.current_quantity, item.unit,
+                                         item.actual_usage, item.target_usage]));
+       }
+       else {
+         console.log(res.data);
+       }
+     })
+     .catch((error) => {
+       console.log(error);
+     });
 
   return (
     <Box>
       {Excess.title}<br/>
-      {startTime.toLocaleUpperCase()}<br/>
-      {getExcessRport(new Date(startTime).toISOString())}
+      {'Start Time: ' + startTime}<br/>
+      <MUIDataTable
+            title={title}
+            data={data}
+            columns={columns}
+            options={options}
+        />
     </Box>
   )
 }
