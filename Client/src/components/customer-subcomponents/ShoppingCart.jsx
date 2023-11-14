bc246ace28a87494a57b5e0958a5ece372d9202c
Donato-Curvino <donato@tamu.edu>
2023-11-14 11:21:46 -0600

    ShoppingCart displays cart items

--- Client/src/components/customer-subcomponents/ShoppingCart.jsx
+++ Client/src/components/customer-subcomponents/ShoppingCart.jsx
@@ -4,10 +4,20 @@ import Dialog from "@mui/material/Dialog"
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+// import ListItemAvatar from '@mui/material/ListItemAvatar';
+import ListItemText from '@mui/material/ListItemText';
+// import Avatar from '@mui/material/Avatar';
+
 import IconButton from '@mui/material/IconButton';
-import { MdClose, MdOutlineShoppingCart} from "react-icons/md";
+import {MdClose, MdOutlineShoppingCart} from "react-icons/md";
+import {BsFillTrash3Fill} from "react-icons/bs";
+import { Icon } from "@mui/material";
 
 function ShoppingCart({open, onClose}) {
+    const cart = JSON.parse(localStorage.getItem("cart"));
+
     return (
         <Dialog
           open={open}
@@ -16,12 +26,24 @@ function ShoppingCart({open, onClose}) {
             <DialogTitle>
                 {/* <MdOutlineShoppingCart/> */}
                 Shopping Cart
-                <IconButton onClick={onClose}>
+                <IconButton onClick={onClose} edge="end">
                     <MdClose/>
                 </IconButton>
             </DialogTitle>
-            <DialogContent>
-
+            <DialogContent dividers>
+                <List> {
+                    cart.map((item) => (
+                        <ListItem 
+                            secondaryAction={(
+                            <IconButton edge="end" size="small">
+                                <BsFillTrash3Fill/>
+                            </IconButton>
+                            )}
+                        >
+                            <ListItemText primary={item.item_name}/>
+                        </ListItem>
+                    ))
+                } </List>
             </DialogContent>
         </Dialog>
     );
