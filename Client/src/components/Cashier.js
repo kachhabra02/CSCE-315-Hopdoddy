5d83b91d55ae738c7b258c11fa52c18243d2b88b
Thomas Holt <tmholt02@gmail.com>
2023-11-27 14:46:58 -0600

    initial styling

--- Client/src/components/Cashier.js
+++ Client/src/components/Cashier.js
@@ -11,102 +11,118 @@ import TransactionList from "./cashier-subcomponents/TransactionList.js";
 import ItemList from "./cashier-subcomponents/ItemList.js";
 import "./cashier-subcomponents/Cashier.css";
 import useAPI from "./useAPI.js";
+import { Box, Button, Grid } from "@mui/material";
+import { Typography } from "@mui/material";
 
 // const API = axios.create({
-//     baseURL: `${process.env.REACT_APP_API_URL}/api`,
-//     timeout: 10000 // 10 second timeout
+//   baseURL: `${process.env.REACT_APP_API_URL}/api`,
+//   timeout: 10000 // 10 second timeout
 // });
 
 function Cashier() {
-    const [orders, setOrders] = useState([]);
-    const [alertStatus, setAlertStatus] = useState({open: false, status: "error"});
+  const [orders, setOrders] = useState([]);
+  const [alertStatus, setAlertStatus] = useState({open: false, status: "error"});
 
-    const [{categories, subcategories, items, currCategory, currSubcategory}, {getItems, getSubcategories}] = useAPI();
+  const [{categories, subcategories, items, currCategory, currSubcategory}, {getItems, getSubcategories}] = useAPI();
 
-    function addOrder(item) {
-        return () => setOrders(orders.concat([item]));
-    }
+  function addOrder(item) {
+    return () => setOrders(orders.concat([item]));
+  }
 
-    function removeOrder(index) {
-        return () => setOrders(orders.toSpliced(index, 1));
-    }
+  function removeOrder(index) {
+    return () => setOrders(orders.toSpliced(index, 1));
+  }
 
-    function placeTransaction() {
-        axios.post(`${process.env.REACT_APP_API_URL}/api/transactions`, {
-            // TODO: change employeeID to an actual ID obtained from logging in
-            employeeID: 2,
-            menuIDs: orders.map((item) => item.item_id)
-        })
-            .then((res) => {
-                if (res.status === 200) {
-                    setOrders([]);
-                    console.log(res);
-                }
-            })
-            .then(() => setAlertStatus({open: true, status: "success"}))
-            .catch(error => {
-                console.log(error);
-                setAlertStatus({open: true, status: "error"})
-            });
-    }
+  function placeTransaction() {
+    axios.post(`${process.env.REACT_APP_API_URL}/api/transactions`, {
+      // TODO: change employeeID to an actual ID obtained from logging in
+      employeeID: 2,
+      menuIDs: orders.map((item) => item.item_id)
+    })
+      .then((res) => {
+        if (res.status === 200) {
+          setOrders([]);
+          console.log(res);
+        }
+      })
+      .then(() => setAlertStatus({open: true, status: "success"}))
+      .catch(error => {
+        console.log(error);
+        setAlertStatus({open: true, status: "error"})
+      });
+  }
 
-    const closeAlert = (event, reason) => {
-        if (reason === "clickaway") {
-            return;
-        }   
-        setAlertStatus({open: false, status: alertStatus.status});
-    }
+  const closeAlert = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }   
+    setAlertStatus({open: false, status: alertStatus.status});
+  }
 
-    return (
-        <div className="Cashier">
-            {/* <NavBar />  */}
-            <h1>This is the Cashier page</h1>
-            <div className="selectionPanel">
-                {(categories === undefined) 
-                    // ? <p>Loading...</p> 
-                    ? <CircularProgress/>
-                    : <CategoryList categories={categories} clickHandler={getSubcategories} selected={currCategory}/>
-                }       
-                {(subcategories === null) 
-                    // ? <p>Loading...</p> 
-                    ? <CircularProgress/> 
-                    : <SubcategoryList subcategories={subcategories} clickHandler={getItems} selected={currSubcategory}/>
-                }
-                {(items === null) 
-                    // ? <p>Loading...</p> 
-                    ? <CircularProgress/> 
-                    : <ItemList items={items} clickHandler={addOrder}/>
-                }
-            </div>
-            <TransactionList orders={orders} remover={removeOrder}/>
-            <div>
-                <button onClick={placeTransaction}>SUBMIT</button>
-                <button 
-                  onClick={() => {
-                    setOrders([]); 
-                    setAlertStatus({open: true, status: "canceled"});
-                  }}
-                >
-                    CANCEL
-                </button>
-            </div>
-            <Snackbar 
-              open={alertStatus.open} 
-              onClose={() => setAlertStatus({open: false, status: alertStatus.status})}
-              autoHideDuration={5000}
-              anchorOrigin={{vertical: "bottom", horizontal: "center"}}
-            //   sx={{width: "500%"}}
+  return (
+    <Box>
+      <Typography variant="h3">
+        Cashier Page
+      </Typography>
+      <Grid container spacing={2}>
+        <Grid item xs={5}>
+          <TransactionList orders={orders} remover={removeOrder}/>
+          <Box>
+            <Button 
+              variant="contained" 
+              color="primary"
+              onClick={placeTransaction}
             >
-                {(alertStatus.status === "success") 
-                    ? <Alert severity="success" sx={{width: "90vw"}} onClose={closeAlert}>Transaction Submitted Sucsessfully!</Alert>
-                    : (alertStatus.status === "canceled")
-                        ? <Alert severity="info" sx={{width: "90vw"}} onClose={closeAlert}>Transaction canceled</Alert>
-                        : <Alert severity="error" sx={{width: "90vw"}} onClose={closeAlert}>Error submitting transaction</Alert>
-                }
-                
-            </Snackbar>
-        </div>
-    );
+              SUBMIT
+            </Button>
+            <Button 
+              variant="contained"
+              color="secondary"
+              onClick={() => {
+                setOrders([]); 
+                setAlertStatus({open: true, status: "canceled"});
+              }}
+            >
+              CANCEL
+            </Button>
+          </Box>
+        </Grid>
+        <Grid item xs={7}>
+          {(categories === undefined) 
+            // ? <p>Loading...</p> 
+            ? <CircularProgress/>
+            : <CategoryList categories={categories} clickHandler={getSubcategories} selected={currCategory}/>
+          }     
+          {(subcategories === null) 
+            // ? <p>Loading...</p> 
+            ? <CircularProgress/> 
+            : <SubcategoryList subcategories={subcategories} clickHandler={getItems} selected={currSubcategory}/>
+          }
+          {(items === null) 
+            // ? <p>Loading...</p> 
+            ? <CircularProgress/> 
+            : <ItemList items={items} clickHandler={addOrder}/>
+          }
+        </Grid>
+      </Grid>
+      
+      <Snackbar 
+        open={alertStatus.open} 
+        onClose={() => setAlertStatus({open: false, status: alertStatus.status})}
+        autoHideDuration={5000}
+        anchorOrigin={{vertical: "bottom", horizontal: "center"}}
+      //   sx={{width: "500%"}}
+      >
+        {(alertStatus.status === "success") 
+          ? <Alert severity="success" sx={{width: "90vw"}} onClose={closeAlert}>Transaction Submitted Sucsessfully!</Alert>
+          : (alertStatus.status === "canceled")
+            ? <Alert severity="info" sx={{width: "90vw"}} onClose={closeAlert}>Transaction canceled</Alert>
+            : <Alert severity="error" sx={{width: "90vw"}} onClose={closeAlert}>Error submitting transaction</Alert>
+        }
+        
+      </Snackbar>
+    </Box>
+  );
 }
 
 export default Cashier;
