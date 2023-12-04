3e39fd480c1a7e600ea6b88edc2f73d34878214f
Donato-Curvino <donato@tamu.edu>
2023-12-03 18:01:35 -0600

    responsive grid sizes correctly on load

--- Client/src/components/customer-subcomponents/ItemGrid.jsx
+++ Client/src/components/customer-subcomponents/ItemGrid.jsx
@@ -1,24 +1,29 @@
-import React, { useLayoutEffect, useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 
 import Grid from '@mui/material/Grid';
 
 import ItemCard from "./ItemCard";
 
 function ItemGrid({items, onUpdate, sx, id}) {
-    const [spacing, setSpacing] = useState(2);
+    const [spacing, setSpacing] = useState(0);
+    const [size, setSize] = useState([0, 0]);
 
     useLayoutEffect(() => {
-        const width = document.getElementById("ItemGrid").getBoundingClientRect().width - 16;
-        const whiteSpace = width % 416
-        const numCards = (width - whiteSpace) / 416
-        // console.log(numCards)
-        // setSpacing(whiteSpace / ((numCards + 1) * 8))
-        setSpacing(whiteSpace / numCards)
-        // console.log(spacing)
-    })
+        function updateSize() {
+            const width = document.getElementById("ItemGrid").getBoundingClientRect().width - 16;
+            setSize([window.innerWidth, window.innerHeight]);
+            const whiteSpace = width % 416;
+            const numCards = (width - whiteSpace) / 416;
+            setSpacing(whiteSpace / numCards);
+        }
+        var timeout = false;
+        window.addEventListener('resize', () => {clearTimeout(timeout); setTimeout(updateSize, 250)});
+        setTimeout(updateSize, 250);
+        return () => window.removeEventListener('resize', updateSize);
+    }, []);
 
     return (
-        <Grid container spacing={2} sx={{...sx, "padding-right": 16, "padding-bottom": 16}} id="ItemGrid">{
+        <Grid container spacing={2} sx={{...sx, "padding-right": 16, "padding-bottom": 16, width: window.innerWidth - 266}} id="ItemGrid">{
             items?.map(item => (
                 <Grid item >
                     <ItemCard item={item} onUpdate={onUpdate} width={400 + spacing}/>
