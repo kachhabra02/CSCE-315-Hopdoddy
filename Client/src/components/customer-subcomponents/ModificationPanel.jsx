a5905e88320e10b96284f1b7a4c0f4c2a8cfed75
Donato-Curvino <donato@tamu.edu>
2023-12-04 17:05:40 -0600

    Initial modification list

--- Client/src/components/customer-subcomponents/ModificationPanel.jsx
+++ Client/src/components/customer-subcomponents/ModificationPanel.jsx
@@ -5,13 +5,26 @@ import Dialog from "@mui/material/Dialog";
 import DialogTitle from '@mui/material/DialogTitle';
 import DialogContent from '@mui/material/DialogContent';
 
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+
+import ItemButton from "../cashier-subcomponents/ItemButton";
+
 function ModificationPanel({open, onClose, modifications}) {
+    console.log(modifications)
 
     return (
         <Dialog open={open} onClose={onClose}>
             <DialogTitle sx={{"min-width": 250}}>
-                {/* {modifications} */}
+                Modifications
             </DialogTitle>
+            <DialogContent dividers>
+                <List> { modifications?.map((item) => (
+                    <ListItem>
+                        <ItemButton width={100} height={50} fontSize={15}>{item.item_name}</ItemButton>
+                    </ListItem>
+                ))} </List>
+            </DialogContent>
         </Dialog>
     ); 
 }
