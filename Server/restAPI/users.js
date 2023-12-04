8c1f445de91f346be55d6ab9edb1287e3d9318e3
Krish Chhabra <kachhabra02@tamu.edu>
2023-12-04 02:59:27 -0600

    Added API endpoints for user management

--- Server/restAPI/users.js
+++ Server/restAPI/users.js
@@ -11,21 +11,166 @@ const queries = require('./queries');
 
 /***** /api/users *****/
 // Get information for all users
-// TODO
+router.get('/', async (req, res) => {
+    // Send query
+    const queryObj = {
+        text: queries.getAllUsersQuery,
+        values: []
+    };
+
+    const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
+
+        res.status(200).json(results.rows);
+    });
+    client.release();
+});
 
 // Add new user
-// TODO
+router.post('/', async (req, res) => {
+    // Get necessary info from request
+    if (!req.body.first_name) {
+        res.status(400).send("Must provide employee's first name (first_name)!");
+        return;
+    }
+
+    if (!req.body.last_name) {
+        res.status(400).send("Must provide employee's last name (last_name)!");
+        return;
+    }
+
+    if (!req.body.email) {
+        res.status(400).send("Must provide employee's email (email)!");
+        return;
+    }
+
+    if (!req.body.is_manager) {
+        res.status(400).send("Must provide manager status (is_manager)!");
+        return;
+    }
+
+    if (!req.body.is_admin) {
+        res.status(400).send("Must provide admin status (is_admin)!");
+        return;
+    }
+
+    // Send Query
+    const queryObj = {
+        text: queries.addUserQuery,
+        values: [req.body.first_name, req.body.last_name, req.body.email, req.body.is_manager, req.body.is_admin]
+    };
+
+    const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
+
+        res.status(200).json(results.rows);
+    });
+    client.release();
+});
 
 
 /***** /api/users/:id *****/
 // Get specific user information
-// TODO
+router.get('/:id', async (req, res) => {
+    // Get necessary info from request
+    const emp_id = req.params.id;
+
+    // Send query
+    const queryObj = {
+        text: queries.getUserQuery,
+        values: [emp_id]
+    };
+
+    const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
+
+        res.status(200).json(results.rows);
+    });
+    client.release();
+});
 
 // Update user information
-// TODO
+router.put('/:id', async (req, res) => {
+    // Get necessary info from request
+    const emp_id = req.params.id;
+
+    if (!req.body.first_name) {
+        res.status(400).send("Must provide employee's first name (first_name)!");
+        return;
+    }
+
+    if (!req.body.last_name) {
+        res.status(400).send("Must provide employee's last name (last_name)!");
+        return;
+    }
+
+    if (!req.body.email) {
+        res.status(400).send("Must provide employee's email (email)!");
+        return;
+    }
+
+    if (!req.body.is_manager) {
+        res.status(400).send("Must provide manager status (is_manager)!");
+        return;
+    }
+
+    if (!req.body.is_admin) {
+        res.status(400).send("Must provide admin status (is_admin)!");
+        return;
+    }
+
+    // Send Query
+    const queryObj = {
+        text: queries.updateUserQuery,
+        values: [req.body.first_name, req.body.last_name, req.body.email, req.body.is_manager, req.body.is_admin, emp_id]
+    };
+
+    const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
+
+        res.status(200).json(results.rows);
+    });
+    client.release();
+});
 
 // Delete user
-// TODO
+router.delete('/:id', async (req, res) => {
+    // Get necessary info from request
+    const emp_id = req.params.id;
+
+    // Send query
+    const queryObj = {
+        text: queries.deleteUserQuery,
+        values: [emp_id]
+    };
+
+    const client = await pool.connect();
+    const result = await client.query(queryObj, (error, results) => {
+        if(error) {
+            res.status(400).send("Error sending query: " + error.message);
+            return;
+        }
+
+        res.status(200).json(results.rows);
+    });
+    client.release();
+});
 
 
 // Export URI router
