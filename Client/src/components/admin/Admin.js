bb53eb2d2b801d135e68818b32e41c560481fa74
Thomas Holt <tmholt02@gmail.com>
2023-12-04 23:02:38 -0600

    Simple editing. Debug backend falsy oversight

--- Client/src/components/admin/Admin.js
+++ Client/src/components/admin/Admin.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import { useState } from 'react';
 
-import { Box } from '@mui/material';
+import { Box, Button, Card, CardActions, CardContent, Modal, Stack, TextField, Typography } from '@mui/material';
 import CircularProgress from '@mui/material/CircularProgress';
 import MUIDataTable from 'mui-datatables';
+
+import EditIcon from '@mui/icons-material/Edit';
+import AddIcon from '@mui/icons-material/Add';
+import ExitToAppIcon from '@mui/icons-material/ExitToApp';
+import StartIcon from '@mui/icons-material/Start';
 
 import axios from 'axios';
 
@@ -11,62 +16,6 @@ const API = axios.create({
   baseURL: `${process.env.REACT_APP_API_URL}/api`,
   timeout: 10000 // 10 second timeout
 });
-
-const title = 'User Administration';
-
-const columns = [
-  {
-    name: 'First',
-    options: {
-      filter: false,
-      sort: true
-    }
-  },
-  {
-    name: 'Last',
-    options: {
-      filter: false,
-      sort: true
-    }
-  },
-  {
-    name: 'Email',
-    options: {
-      filter: true,
-      sort: true
-    }
-  },
-  {
-    name: 'Permission Level',
-    options: {
-      filter: false,
-      sort: true,
-      customBodyRender: ({ isManager, isAdmin }) => {
-        const permissionLevel = (() => {
-          if (isAdmin)
-            return 'Admin';
-          else if (isManager) 
-            return 'Manager';
-          else 
-            return 'Cashier';
-        })();
-        return (
-          <div>{permissionLevel}</div>
-        );
-      }
-    }
-  },
-  {
-    name: 'Actions',
-    options: {
-      filter: false,
-      sort: true,
-      customBodyRender: data => {
-        return data;
-      }
-    }
-  }
-];
 
 const options = {
   filterType: 'multiselect',
@@ -78,55 +27,217 @@ const options = {
 
 
 function Admin() {
-  const [data, setData] = useState([
-    ['Thomas', 'Holt', 'tmholt02@gmail.com', { isAdmin: 1 }, "Actions!!"],
-    ['Donato', 'Curvino', 'dn@gmail.com', { isManager: 1 }, "Actions!!"],
-    ['Ryan', 'Pohl', 'rp@gmail.com', {}, "Actions!!"],
-  ]);
+  const [rows, setRows] = useState(null);
+  const [currUser, setCurrUser] = useState(null);
+  const [isEditing, setIsEditing] = useState(false);
 
-  // React.useEffect(() => {
-  //   getUsers()
-  //     .then(data => {
-  //       setData(data);
-  //     });
-  // }, []);
+  React.useEffect(() => {
+    getUsers()
+      .then(rows => {
+        setRows(rows);
+      });
+  }, []);
+
+  const handleEditUser = (user) => {
+    setCurrUser(user);
+    setIsEditing(true);
+  }
+
+  const handleEditClose = () => {
+    setCurrUser(null);
+    setIsEditing(false);
+  }
+
+  const handleEditSave = () => {
+    updateUser(currUser)
+      .then(() => {
+        setCurrUser(null);
+        setIsEditing(false);
+        return getUsers();
+      })
+      .then(rows => {
+        setRows(rows);
+      });
+  }
+
+  const handleChange = (e) => {
+    const { name, value, type, checked } = e.target;
+    setCurrUser(prevState => ({
+        ...prevState,
+        [name]: type === 'checkbox' ? checked : value
+    }));
+  }
+
+  const title = 'User Administration';
+
+  const columns = [
+    {
+      name: 'First',
+      options: {
+        filter: false,
+        sort: true
+      }
+    },
+    {
+      name: 'Last',
+      options: {
+        filter: false,
+        sort: true
+      }
+    },
+    {
+      name: 'Email',
+      options: {
+        filter: true,
+        sort: true
+      }
+    },
+    {
+      name: 'Permission Level',
+      options: {
+        filter: false,
+        sort: true
+      }
+    },
+    {
+      name: 'Actions',
+      options: {
+        filter: false,
+        sort: true,
+        customBodyRender: data => {
+          return (
+            <Button
+              onClick={() => handleEditUser(data)}
+              variant='outlined'
+              color='warning'
+              startIcon={<EditIcon />}
+              children={'Edit user'}
+            />
+          );
+        }
+      }
+    }
+  ];
 
   return (
     <Box>
-      {data === undefined ? <CircularProgress /> :
+      {!rows ? <CircularProgress /> :
         <MUIDataTable
           title={title}
-          data={data}
+          data={rows}
           columns={columns}
           options={options}
         />
       }
+      {isEditing && (
+        <Modal
+          open={isEditing}
+          aria-labelledby="edit-modal"
+          aria-describedby="edit-modal-changes-selected-user"
+        >
+          <Box sx={{
+            display: 'flex',          // Enable flex container
+            flexDirection: 'column',  // Stack children vertically
+            justifyContent: 'center', // Center children along the vertical axis
+            alignItems: 'center',     // Center children along the horizontal axis
+            height: '80vh',           // Set the height of the container (e.g., full viewport height)
+          }}>
+            <Card sx={{ minWidth: 345 }}>
+              <CardContent>
+                <Typography variant='h6' paddingBottom={3}>
+                  Edit User
+                </Typography>
+                <Stack spacing={4}>
+                  <TextField 
+                    label="First Name" 
+                    name="first_name" 
+                    value={currUser.first_name} 
+                    onChange={handleChange} 
+                    margin="normal"
+                    fullWidth
+                  />
+                  <TextField 
+                    label="Last Name" 
+                    name="last_name" 
+                    value={currUser.last_name} 
+                    onChange={handleChange} 
+                    margin="normal"
+                    fullWidth
+                  />
+                  <TextField 
+                    label="Email" 
+                    name="email" 
+                    type="email"
+                    value={currUser.email} 
+                    onChange={handleChange} 
+                    margin="normal"
+                    fullWidth
+                  />
+                </Stack>
+              </CardContent>
+              <CardActions>
+                <Box sx={{ textAlign: 'left', flexGrow: 1 }}>    
+                  <Button 
+                    variant='contained' 
+                    color='success' 
+                    onClick={handleEditSave}
+                    children={<EditIcon />}
+                  />
+                </Box>
+                <Button 
+                  variant='contained' 
+                  color='error' 
+                  onClick={handleEditClose}
+                  children={<ExitToAppIcon />}
+                />
+              </CardActions>
+            </Card>
+          </Box>
+        </Modal>
+      )}
     </Box>
   );
 }
 
-// async function getUsers() {
-//   try {
-//     const res = await API.get(`/users`);
-//     if (res.status < 300) {
-//       console.log('Success');
-//       return (res.data.map((item) => [
-//         item.first_name,
-//         item.last_name,
-//         item.email,
-//         item.is_manager,
-//         item.is_admin
-//       ]));
-//     }
-//     else {
-//       console.log(res.data);
-//       return ['Error Retrieving Report! Please try again... (may need to use a smaller time window)'];
-//     }
-//   }
-//   catch (error) {
-//     console.log(error);
-//     return ['Error Retrieving Report! Please try again... (may need to use a smaller time window)'];
-//   }
-// }
+async function getUsers() {
+  try {
+    const res = await API.get(`/users`);
+    if (res.status < 300) {
+      return (res.data.map((item) => [
+        item.first_name,
+        item.last_name,
+        item.email,
+        (() => {
+          if (item.is_admin)
+            return 'Admin';
+          else if (item.is_manager) 
+            return 'Manager';
+          else 
+            return 'Cashier';
+        })(),
+        item
+      ]));
+    }
+    else {
+      console.log(res.data);
+      return ['Error Retrieving Report! Please try again... (may need to use a smaller time window)'];
+    }
+  }
+  catch (error) {
+    console.log(error);
+    return ['Error Retrieving Report! Please try again... (may need to use a smaller time window)'];
+  }
+}
+
+async function updateUser(user) {
+  console.log(user);
+  try {
+    const res = await API.put(`/users/${user.employee_id}`, user);
+    console.log(res);
+  }
+  catch (error) {
+    console.log(error);
+  }
+}
 
 export default Admin
