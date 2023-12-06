30bbb9b626c9a126507a9b218a4aa44fbda1156a
Donato-Curvino <donato@tamu.edu>
2023-12-05 22:13:44 -0600

    fixed itemGrid resizing because it broke

--- Client/src/components/customer-subcomponents/ItemGrid.jsx
+++ Client/src/components/customer-subcomponents/ItemGrid.jsx
@@ -6,15 +6,16 @@ import ItemCard from "./ItemCard";
 
 function ItemGrid({items, onUpdate, sx, modifier}) {
     const [spacing, setSpacing] = useState(0);
-    // const [size, setSize] = useState([0, 0]);
+    const [size, setSize] = useState([0, 0]);
 
     useLayoutEffect(() => {
         function updateSize() {
             const width = document.getElementById("ItemGrid").getBoundingClientRect().width - 16;
-            // setSize([window.innerWidth, window.innerHeight]);
+            setSize([window.innerWidth, window.innerHeight]);
             const whiteSpace = width % 416;
             const numCards = (width - whiteSpace) / 416;
             setSpacing(whiteSpace / numCards);
+            // console.log(width)
         }
         var timeout = false;
         window.addEventListener('resize', () => {clearTimeout(timeout); setTimeout(updateSize, 250)});
@@ -23,7 +24,7 @@ function ItemGrid({items, onUpdate, sx, modifier}) {
     }, []);
 
     return (
-        <Grid container spacing={2} sx={{...sx, paddingRight: 16, paddingBottom: 16, width: window.innerWidth - 266}} id="ItemGrid">{
+        <Grid container spacing={2} sx={{...sx, width: size[0] - 266}} id="ItemGrid">{
             items?.map((item, i) => (
                 <Grid item key={i}>
                     <ItemCard item={item} onUpdate={onUpdate} width={400 + spacing} modifier={modifier(item.item_id )}/>
