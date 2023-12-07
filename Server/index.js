096d869fa305dcebbf3724ec0aa5b55c3a1dccd4
Krish Chhabra <kachhabra02@tamu.edu>
2023-12-06 18:55:58 -0600

    Try adding in object

--- Server/index.js
+++ Server/index.js
@@ -5,7 +5,7 @@ const app = express();
 // Swagger Docs Requirements
 const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./swagger.json');
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument, {}));
 
 // Configure port to listen on
 require('dotenv').config();
